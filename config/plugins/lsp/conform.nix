c78e733b212c115006ee87638259a40a2377d70b
Hier0nim <hieronimdaniel@gmail.com>
2024-10-19 08:49:57 +0200

    format conform.nix

--- config/plugins/lsp/conform.nix
+++ config/plugins/lsp/conform.nix
@@ -1,4 +1,5 @@
-{ lib, pkgs, ... }: {
+{ lib, pkgs, ... }:
+{
   config = {
     extraConfigLuaPre =
       # lua
@@ -38,68 +39,134 @@
     plugins.conform-nvim = {
       enable = true;
       settings = {
-        format_on_save = ''
-          function(bufnr)
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-              return
-            end
-
-            if slow_format_filetypes[vim.bo[bufnr].filetype] then
-              return
-            end
-
-            local function on_format(err)
-              if err and err:match("timeout$") then
-                slow_format_filetypes[vim.bo[bufnr].filetype] = true
+        format_on_save =
+          # lua
+          ''
+            function(bufnr)
+              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
               end
+
+              if slow_format_filetypes[vim.bo[bufnr].filetype] then
+                return
+              end
+
+              local function on_format(err)
+                if err and err:match("timeout$") then
+                  slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                end
+              end
+
+              return { timeout_ms = 200, lsp_fallback = true }, on_format
+             end
+          '';
+
+        format_after_save =
+          # lua
+          ''
+            function(bufnr)
+              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+              end
+
+              if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+                return
+              end
+
+              return { lsp_fallback = true }
             end
-
-            return { timeout_ms = 200, lsp_fallback = true }, on_format
-           end
-        '';
-
-        format_after_save = ''
-          function(bufnr)
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-              return
-            end
-
-            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-              return
-            end
-
-            return { lsp_fallback = true }
-          end
-        '';
+          '';
         notify_on_error = true;
         formatters_by_ft = {
-          html = [[ "prettierd" "prettier" ]];
-          css = [[ "prettierd" "prettier" ]];
-          javascript = [[ "prettierd" "prettier" ]];
-          typescript = [[ "prettierd" "prettier" ]];
-          python = [ "black" "isort" ];
+          html = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          css = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          javascript = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          typescript = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          python = [
+            "black"
+            "isort"
+          ];
           lua = [ "stylua" ];
           nix = [ "nixfmt" ];
-          markdown = [[ "prettierd" "prettier" ]];
-          yaml = [[ "prettierd" "prettier" ]];
+          markdown = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          yaml = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
           terraform = [ "terraform_fmt" ];
           bicep = [ "bicep" ];
-          bash = [ "shellcheck" "shellharden" "shfmt" ];
+          bash = [
+            "shellcheck"
+            "shellharden"
+            "shfmt"
+          ];
           json = [ "jq" ];
           "_" = [ "trim_whitespace" ];
         };
 
         formatters = {
-          black = { command = "${lib.getExe pkgs.black}"; };
-          isort = { command = "${lib.getExe pkgs.isort}"; };
-          nixfmt = { command = "${lib.getExe pkgs.nixfmt-rfc-style}"; };
-          jq = { command = "${lib.getExe pkgs.jq}"; };
-          prettierd = { command = "${lib.getExe pkgs.prettierd}"; };
-          stylua = { command = "${lib.getExe pkgs.stylua}"; };
-          shellcheck = { command = "${lib.getExe pkgs.shellcheck}"; };
-          shfmt = { command = "${lib.getExe pkgs.shfmt}"; };
-          shellharden = { command = "${lib.getExe pkgs.shellharden}"; };
-          bicep = { command = "${lib.getExe pkgs.bicep}"; };
+          injected = {
+            options = {
+              ignore_errors = true;
+            };
+          };
+          black = {
+            command = "${lib.getExe pkgs.black}";
+          };
+          isort = {
+            command = "${lib.getExe pkgs.isort}";
+          };
+          nixfmt = {
+            command = "${lib.getExe pkgs.nixfmt-rfc-style}";
+          };
+          jq = {
+            command = "${lib.getExe pkgs.jq}";
+          };
+          prettierd = {
+            command = "${lib.getExe pkgs.prettierd}";
+          };
+          stylua = {
+            command = "${lib.getExe pkgs.stylua}";
+          };
+          shellcheck = {
+            command = "${lib.getExe pkgs.shellcheck}";
+          };
+          shfmt = {
+            command = "${lib.getExe pkgs.shfmt}";
+          };
+          shellharden = {
+            command = "${lib.getExe pkgs.shellharden}";
+          };
+          bicep = {
+            command = "${lib.getExe pkgs.bicep}";
+          };
           #yamlfmt = {
           #  command = "${lib.getExe pkgs.yamlfmt}";
           #};
