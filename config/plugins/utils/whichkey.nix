142dee381a43c2a39c5cb05c6ab5488f85542022
Hier0nim <hieronimdaniel@gmail.com>
2024-10-19 08:48:57 +0200

    add groups for whichkey

--- config/plugins/utils/whichkey.nix
+++ config/plugins/utils/whichkey.nix
@@ -1,5 +1,67 @@
 {
   plugins.which-key = {
     enable = true;
+    settings = {
+      preset = "helix";
+      spec = [
+        {
+          __unkeyed = "<leader>c";
+          group = "Code";
+          mode = [
+            "n"
+            "v"
+          ];
+        }
+        {
+          __unkeyed = "<leader>g";
+          group = "Git";
+          mode = [
+            "n"
+          ];
+        }
+        {
+          __unkeyed = "<leader>f";
+          group = "Find";
+          mode = [
+            "n"
+          ];
+        }
+        {
+          __unkeyed = "<leader>m";
+          group = "Markdown";
+          mode = [
+            "n"
+          ];
+        }
+        {
+          __unkeyed = "<leader>q";
+          group = "Session";
+          mode = [
+            "n"
+          ];
+        }
+        {
+          __unkeyed = "<leader>s";
+          group = "Search";
+          mode = [
+            "n"
+          ];
+        }
+        {
+          __unkeyed = "<leader>u";
+          group = "UI";
+          mode = [
+            "n"
+          ];
+        }
+        {
+          __unkeyed = "<leader>w";
+          group = "Window";
+          mode = [
+            "n"
+          ];
+        }
+      ];
+    };
   };
 }
